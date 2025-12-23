ae65572d2fcf9ae58cac05bf3a0c7bc558995810
Runedp1 <rune.de.peet@gmail.com>
2025-12-23 13:50:49 +0100

    Downgrade to .NET 8.0 for Azure compatibility - Fix MapStaticAssets

--- frontend/CarRecommender.Web/Program.cs
+++ frontend/CarRecommender.Web/Program.cs
@@ -27,12 +27,12 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
 
 app.Run();
